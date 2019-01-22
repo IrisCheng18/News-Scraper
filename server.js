b442f3028e81c8af99ebbfa00b518d6ce314ba6b
Iris Cheng <chengiris66@gmail.com>
2019-01-22 09:37:39 -0800

    changes

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.set('view engine', 'handlebars');
 
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = (process.env.MONGODB_URI || 'mongodb://localhost:27017/mongoHeadlines');
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
